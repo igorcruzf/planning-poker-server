db539fc8d6eac959f16b40d06d2337d8b7854df7
igfigueiredo <igfigueiredo@uolinc.com>
2023-06-29 13:15:28 -0300

    feat - added room handling

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,5 +1,6 @@
 import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect, MessageBody, ConnectedSocket} from '@nestjs/websockets';
 import {Logger} from "@nestjs/common";
+import {Socket} from "socket.io";
 
 @WebSocketGateway( {
     cors: {
@@ -25,7 +26,8 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
         const index = this.users.findIndex((f) => client === f[0]);
         this.logger.log("Client with index: ", index)
         if(index !== -1) {
-            this.server.emit('logout', this.users[index][1])
+            const user = this.users[index][1]
+            this.server.to(user.room).emit('logout', user)
             this.users = [...this.users.slice(0, index), ...this.users.slice(index + 1)];
         }
     }
@@ -35,30 +37,39 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
     }
 
     @SubscribeMessage('login')
-    handleLoginEvent(@ConnectedSocket() client, @MessageBody() userInfo: any) {
+    handleLoginEvent(@ConnectedSocket() client: Socket, @MessageBody() userInfo: any) {
+        client.join(userInfo.room)
         this.logger.log('Received login:', JSON.stringify(userInfo));
         if(this.users.filter( (user) => user[1].id === userInfo.id).length === 0) {
             this.users.push([client, userInfo]);
         }
-        client.emit('start', this.users.map( (user) => user[1]))
+        const usersInSameRoom = this.users.filter( (user) => user[1].room === userInfo.room ).map( (user) => user[1])
+        this.logger.log('users in same room:', JSON.stringify(usersInSameRoom))
+        client.emit('start', usersInSameRoom)
         if(this.reveal) {
             client.emit('startReveal', true)
         }
-        this.server.emit('login', userInfo);
+        this.server.to(userInfo.room).emit('login', userInfo);
     }
 
     @SubscribeMessage('update')
     handleUpdateEvent(@ConnectedSocket() client, @MessageBody() userInfo: any) {
         this.logger.log('Received update:', JSON.stringify(userInfo));
         this.users = this.addOrUpdateField(client, userInfo)
-        this.server.emit('update', userInfo);
+        this.server.to(userInfo.room).emit('update', userInfo);
     }
 
     @SubscribeMessage('reveal')
     handleRevealEvent(@ConnectedSocket() client, @MessageBody() shouldReveal: boolean) {
         this.logger.log('Received reveal event:', shouldReveal);
-        this.reveal = shouldReveal
-        this.server.emit('reveal', shouldReveal);
+        const index = this.users.findIndex((f) => client === f[0]);
+        this.logger.log("Client with index: ", index)
+        if(index !== -1) {
+            const user = this.users[index][1]
+            this.reveal = shouldReveal
+            this.server.to(user.room).emit('reveal', shouldReveal);
+        }
+
     }
     addOrUpdateField(
         client: any,
